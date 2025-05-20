09d8ea20a46738475920ff0aa97e2021896df32f
somaye khaleghi <SomayeKhaleghi@gmail.com>
2025-05-20 12:19:17 +0330

    using Hilt’s to build the DB
    remove  duplication issue in my setup

--- app/src/main/java/com/challenge/catbreedexplorer/data/local/CatDatabase.kt
+++ app/src/main/java/com/challenge/catbreedexplorer/data/local/CatDatabase.kt
@@ -5,24 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [CatBreedEntity::class], version = 5, exportSchema = false)
+@Database(entities = [CatBreedEntity::class], version = 6, exportSchema = false)
 abstract class CatDatabase : RoomDatabase() {
     abstract fun catBreedDao(): CatBreedDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: CatDatabase? = null
-
-        fun getInstance(context: Context): CatDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    CatDatabase::class.java,
-                    "cat_breeds_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
